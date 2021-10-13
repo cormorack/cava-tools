616051c2900a0b3bd8ec1ff8cbc002ec46748494
lsetiawan <landungs@uw.edu>
2021-10-13 16:15:05 -0700

    Expose api module in discrete_summary

---
+++ cava_tools/discrete_summary/__init__.py
@@ -0,0 +1,15 @@
+# fmt: off
+from cava_tools.discrete_summary.api import (filter_contents, get_contents,
+                                             get_latest_content,
+                                             read_and_clean,
+                                             split_summary_data)
+
+# fmt: on
+
+__all__ = [
+    "get_contents",
+    "filter_contents",
+    "get_latest_content",
+    "read_and_clean",
+    "split_summary_data",
+]
